4db1ae8426003fd6bb9f4fac07691ff4fe8d4b9a
nikolai2787 <nikolayytaran@gmail.com>
2022-05-24 01:19:38 +0300

    Ver.0.3

--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.cafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    private EditText editTextTextPersonName;
+    private EditText editTextTextPassword;
+    private Button buttonRegister;
+    private TextView goToRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        buttonRegister = findViewById(R.id.buttonRegister);
+        goToRegister = findViewById(R.id.goToRegister);
+
+        Intent intentFee = new Intent(this, CreateOrderActivity.class);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextTextPersonName.getText().toString().trim();
+                String password = editTextTextPersonName.getText().toString().trim();
+                if (!name.isEmpty() && !password.isEmpty()) {
+                    Intent intentEntrance = new Intent(getApplicationContext(), CreateOrderActivity.class);
+                    intentEntrance.putExtra("name", name);
+                    intentEntrance.putExtra("password", password);
+                    startActivity(intentEntrance);
+                } else {
+                    Toast.makeText(MainActivity.this, R.string.warning_fill_fields, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
